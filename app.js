b1f4b7ea304dc766e7c453041b859db63077fc70
Tarandeep Singh <65854945+Tarandeep-s1ngh@users.noreply.github.com>
2021-08-15 01:05:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ const noOfNotes= document.querySelectorAll(".noOfNotes");
 
 const arrayNoteAmt = [2000, 500, 100, 20, 10, 5, 1];
 
-
-//if bill amt filled, display cash given input field
 nextBtn.addEventListener('click', ()=>{
     hideError();
     if(Number(billAmt.value)>0){
@@ -30,12 +28,9 @@ nextBtn.addEventListener('click', ()=>{
     }
 } )
 
-
-//check btn clicked handler
 checkBtn.addEventListener('click', ()=>{
     clearNoOfNotes();
     hideError();
-    //error handling
     let billAmtValue= Number(billAmt.value);
     let cashRecievedValue= Number(cashRecieved.value);
 
@@ -49,14 +44,12 @@ checkBtn.addEventListener('click', ()=>{
             showError("Cash recieved is less than bill, please collect more cash");
             return;
         }
-        //if input valid calculate no. of notes
         calculateNotes(billAmtValue, cashRecievedValue);
     } else{
         showError("Enter valid bill amount and cash recieved to continue");
         }
 })
 
-//to calculate no. of notes
 function calculateNotes(bill, cash){
     let returnAmt = cash-bill;
     
@@ -72,7 +65,6 @@ function calculateNotes(bill, cash){
     
 }
 
-//compare with currency and post the no. of notes on screen
 function compare(remainder, noteAmt, index){
 
     if(remainder >= noteAmt){
@@ -83,7 +75,6 @@ function compare(remainder, noteAmt, index){
     return remainder
 }
 
-//if check button clicked without refreshing the page, clear the no of notes values on the screen
 function clearNoOfNotes(){
     for(let notes of noOfNotes){
         notes.innerText = "";
